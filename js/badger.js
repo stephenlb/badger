6089dd9f41c4eb58b0c512c4a2c200dd5453f1b1
Stephen Blum <blum.stephen@gmail.com>
2018-06-14 10:44:04 -0700

    badger badger badger.

---
+++ js/badger.js
@@ -0,0 +1,24 @@
+(async _=>{ 'use strict';
+
+// Subscribe to Badger Channel
+subscribe({
+    channel : 'badger'
+,   message : message
+});
+
+// Receive Badger Badger Badger Badger Badger
+function message(payload) {
+}
+
+// Display Badger Badger Badger Badger Badger
+function badger() {
+}
+
+// Show an Animated Gif
+function show() {
+    const img = document.createElemnt('img');
+    img.src = 
+
+}
+
+})();
